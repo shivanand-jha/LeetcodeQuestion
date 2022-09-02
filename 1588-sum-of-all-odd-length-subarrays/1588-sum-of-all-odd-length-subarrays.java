5c57ebebf182dd8e6470005733ca0b08387fbaa8
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2022-09-02 13:48:06 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (5.59%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+     int sum=0;
+        int n=arr.length;
+        for(int i=0; i<arr.length; ++i)
+        {
+            sum  += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        }
+        return sum;
+    }
+}
