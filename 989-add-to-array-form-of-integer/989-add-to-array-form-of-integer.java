6a72191e5f289f5ab6d2c90a8930edb424c990cf
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2022-09-17 23:22:21 +0530

    Time: 8 ms (66.69%), Space: 62.8 MB (18.69%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
@@ -1,18 +1,14 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-     LinkedList<Integer> result = new LinkedList<>();
-        int len = num.length - 1;
-        
-        while(len >= 0 || k != 0){
-            
-            if(len >= 0){
-                k += num[len--];
+     LinkedList<Integer> result = new LinkedList<>();//linked list of type integer so that we can add at first position of list
+        int len = num.length - 1;//length of array 
+        while(len >= 0 || k != 0){ 
+            if(len >= 0){ //
+                k += num[len--]; //Directly add to digit of k
             }
-            
-            result.addFirst(k % 10);
+            result.addFirst(k % 10);//add at first
             k /= 10;
-        }
-            
+        }   
         return result;
     }
 }
