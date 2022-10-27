03995c7045363e57709859f15e389517799552c2
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2022-10-27 07:36:53 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class gfg
+{
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String s[] = read.readLine().trim().split("\\s+");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            
+            int arr[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = Integer.parseInt(st[i]);
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.searchInSorted(arr, n, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        
+        // Your code here
+        int l=0;
+        while(l<=N)
+        {
+            int mid = l+(N-l)/2;
+            if(K>arr[mid])
+            {
+                l=mid+1;
+            }
+            else if(K<arr[mid])
+            {
+                N=mid-1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        return -1;
+    }
+    
+}
