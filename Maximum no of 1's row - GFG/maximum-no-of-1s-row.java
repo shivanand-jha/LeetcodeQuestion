68a47574d10fe303c7ab05d7c3b6cca73df7b0ef
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2022-11-02 23:18:58 +0530

    Added solution - LeetHub

---
+++ Maximum no of 1's row - GFG/maximum-no-of-1s-row.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int [][]arr = new int[n][m];
+            
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                    arr[i][j] = sc.nextInt ();
+
+    		System.out.println (new Sol().maxOnes (arr, n, m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    public static int maxOnes (int Mat[][], int N, int M)
+    {
+        // your code here
+        int a,max=Integer.MIN_VALUE;
+        int re=0;
+        for(int i=0; i<N; i++)
+        {
+            a=0;
+            for(int j=0; j<M; j++)
+            {
+                if(Mat[i][j]==1)
+                {
+                    a=M-j;
+                    if(a>max){
+                        re=i;
+                        max=a;
+                    }
+                    break;
+                }
+            }
+        }
+        return re;
+    }
+}
