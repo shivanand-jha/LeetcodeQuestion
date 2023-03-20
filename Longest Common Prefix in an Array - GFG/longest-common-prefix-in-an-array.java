bc7ee73887e40ae0541e0449a89b8f0fa7c8d617
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2023-03-20 18:33:37 +0530

    Added solution - LeetHub

---
+++ Longest Common Prefix in an Array - GFG/longest-common-prefix-in-an-array.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine().trim());
+            String arr[] = read.readLine().trim().split(" ");
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonPrefix(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    String longestCommonPrefix(String strs[], int n){
+        if(strs == null || strs.length == 0) return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0,pre.length()-1);
+            i++;
+        }
+      return pre.length()==0?"-1":pre;
+    }
+}
